85070b1f6f805d52ff04b590743ab8876b0f8b00
masacoco <masaonakama0329@gmail.com>
2021-08-08 17:54:58 +0000

    [add]edit,back button

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,33 @@
 class BooksController < ApplicationController
+  
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
-
-  def new
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
-
+  
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(blog_params)
+    redirect_to book_path(book)
+  end
+  
+private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end  
+  
 end
