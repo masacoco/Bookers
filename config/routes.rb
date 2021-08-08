f7d9f7673af14e14fd8a60612ea4b48a1fba51c7
masacoco <masaonakama0329@gmail.com>
2021-08-08 15:23:35 +0000

    [ADD]top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'top' => 'homes#top'
+  
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
