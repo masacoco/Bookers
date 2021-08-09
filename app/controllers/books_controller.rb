87087f5f29b5e95ae738e8078c03dc6dbe33e4d3
masacoco <masaonakama0329@gmail.com>
2021-08-09 09:46:20 +0000

    [Add]validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,6 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
-  end
-  
   def show
     @book = Book.find(params[:id])
   end
@@ -21,8 +15,21 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(params[:id])
+    end
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book)
+    else 
+     @books = Book.all
+     render :index
+    end
   end
   
   def destroy
